98b5d9003e824af77504b9be62b56207f7d38e99
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-09-12 16:53:08 -0500

    se agrega proxy.conf.json para superar error de CORS, y se editan algunos archivos para esto.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  webApiBase: 'https://e7ab2689-0ed3-4f68-a475-23557a86226e.mock.pstmn.io',
+  //webApiBase: 'https://e7ab2689-0ed3-4f68-a475-23557a86226e.mock.pstmn.io',
+  webApiBase: 'http://localhost:3000/api',
   azure: `https://ussouthcentral.services.azureml.net/workspaces/
     67e04638658949d8a650452a02b98bdb/services/06c5630cea5f4c8582fc75ef66b228ab/execute?api-version=2.0&format=swagger`
 };
